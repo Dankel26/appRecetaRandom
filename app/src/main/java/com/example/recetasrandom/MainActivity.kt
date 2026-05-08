b80ec3e3d2e19a53fc174afe25ddeaa566b9723b
Sala A15 <CSalaA15@uniboyaca.edu.co>
2026-05-08 18:52:09 -0500

    ajuste de pantalla principal

--- app/src/main/java/com/example/recetasrandom/MainActivity.kt
+++ app/src/main/java/com/example/recetasrandom/MainActivity.kt
@@ -6,18 +6,25 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.recetasrandom.ui.theme.RecetasRandomTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,37 +39,38 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun InicioApp(){
     Column(
-        modifier = Modifier.padding(24.dp).fillMaxSize(),
+        modifier = Modifier.padding(50.dp).fillMaxWidth(),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
+        horizontalAlignment = Alignment.CenterHorizontally
+        )
+    {
+        Spacer(modifier = Modifier.height(300.dp))
 
-        ) {
         Text(
             modifier = Modifier.fillMaxWidth(),
-            text = "Hola mundo!!!!",
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+            fontSize = 24.sp,
+            style = MaterialTheme.typography.headlineMedium,
+            text = "Recetas Random",
         )
+        Spacer(modifier = Modifier.height(12.dp))
+
         Text(
-            text = "usuario",
-        )
-        TextField(
+            text = "Presiona el botón para obtener una deliciosa receta seleccionada al azar.",
             modifier = Modifier.fillMaxWidth(),
-            value = "",
-            onValueChange = {}
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.onSurfaceVariant // Un color un poco más suave
         )
-        Text (text = "password")
-        TextField(
-            modifier = Modifier.fillMaxWidth(),
-            label ={
-                Text (text = "password label")
-            },
-            value = "",
-            onValueChange = {}
-        )
+
+        Spacer(modifier = Modifier.height(12.dp))
+
         Button(
             modifier = Modifier.fillMaxWidth(),
             onClick = {}
         ) {
-            Text(text = "iniciar sesion")
+            Text(text = "Me siento con suerte")
         }
     }
 }
